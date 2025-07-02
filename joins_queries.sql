1a07ba88b795ca9a9958cebcbae94ade8e9061bd
Brian Kuria <12995.2020@students.ku.ac.ke>
2025-07-02 14:06:43 +0300

    added order by

--- joins_queries.sql
+++ joins_queries.sql
@@ -21,6 +21,8 @@ LEFT JOIN
     reviews AS reviews
     ON
     property.property_id = review.property_id
+ORDER BY 
+    review.ratings
 
 /*Write a query using a FULL OUTER JOIN to retrieve all users and all bookings, even if the user has
 no booking or a booking is not linked to a user.*/
