bf1def90bef28d49dbce857c17dac30c7bb8c972
Brian Kuria <12995.2020@students.ku.ac.ke>
2025-07-02 13:45:54 +0300

    Task 0:Write Complex Queries with Joins

---
+++ README.md
@@ -0,0 +1,10 @@
+# SQL JOIN Queries Documentation – Airbnb Clone
+
+## INNER JOIN – Bookings and Users
+Retrieves all bookings along with the details of the users who made those bookings. Only records with matching users and bookings are returned.
+
+## LEFT JOIN – Properties and Reviews
+Retrieves all properties and their associated reviews. Includes properties even if they do not have any reviews, ensuring a complete property listing view.
+
+## FULL OUTER JOIN – Users and Bookings
+Retrieves all users and all bookings. Includes users without bookings and bookings not linked to any user. Useful for comprehensive reporting of users and booking records in the system.
---
+++ joins_queries.sql
@@ -0,0 +1,35 @@
+/*Write a query using an INNER JOIN to retrieve all bookings and the respective users who made 
+those bookings.*/
+
+SELECT
+    *
+FROM
+    bookings AS bookings
+INNER JOIN
+    users AS users
+    ON
+    bookings.user_id = users.user_id
+
+/*Write a query using a LEFT JOIN to retrieve all properties and their reviews, including properties
+ that have no reviews.*/
+
+SELECT
+    *
+FROM
+    property AS property
+LEFT JOIN
+    reviews AS reviews
+    ON
+    property.property_id = review.property_id
+
+/*Write a query using a FULL OUTER JOIN to retrieve all users and all bookings, even if the user has
+ no booking or a booking is not linked to a user.*/
+
+ SELECT
+    *
+FROM
+    users AS users
+FULL OUTER JOIN
+    bookings AS bookings
+    ON
+    users.user_id = bookings.user_id
