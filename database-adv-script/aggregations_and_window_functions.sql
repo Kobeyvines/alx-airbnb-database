63d490ef8fd4d7689ab2385a4583828ab6999a02
Brian Kuria <12995.2020@students.ku.ac.ke>
2025-07-02 15:30:32 +0300

    task 2:001

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -38,3 +38,21 @@ JOIN
 GROUP BY 
     p.name,
     p.location
+
+--ROW NUMBER
+
+SELECT
+    p.property_id,
+    p.name,
+    p.location,
+    COUNT(b.booking_id) AS total_number_of_bookings,
+    ROW_NUMBER() OVER (ORDER BY COUNT(b.booking_id) DESC) AS booking_row_number
+FROM
+    properties p
+LEFT JOIN
+    bookings b
+    ON p.property_id = b.property_id
+GROUP BY
+    p.property_id,
+    p.name,
+    p.location;
